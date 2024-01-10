1d2b269f304d6ac1402b1a083f3f6552aaa31a72
a <EYCHFSJD@gmail.com>
2024-01-11 01:00:47 +0800

    basically done

--- src/main/java/com/keletu/ancient_thaumcraft/client/render/TileAlchemyFurnaceAdvancedRenderer.java
+++ src/main/java/com/keletu/ancient_thaumcraft/client/render/TileAlchemyFurnaceAdvancedRenderer.java
@@ -1,24 +1,33 @@
 package com.keletu.ancient_thaumcraft.client.render;
 
 import com.keletu.ancient_thaumcraft.tile.TileAlchemyFurnaceAdvanced;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
+import thaumcraft.api.blocks.BlocksTC;
 import thaumcraft.client.lib.obj.AdvancedModelLoader;
 import thaumcraft.client.lib.obj.IModelCustom;
 
 @SideOnly(value= Side.CLIENT)
-public class TileAlchemyFurnaceAdvancedRenderer extends TileEntitySpecialRenderer<TileAlchemyFurnaceAdvanced> {
+public class TileAlchemyFurnaceAdvancedRenderer
+extends TileEntitySpecialRenderer<TileAlchemyFurnaceAdvanced> {
     private final IModelCustom model = AdvancedModelLoader.loadModel(FURNACE);
     private static final ResourceLocation FURNACE = new ResourceLocation("ancient_thaumcraft", "textures/models/adv_alch_furnace.obj");
 
     @Override
-    public void render(TileAlchemyFurnaceAdvanced tile, double par2, double par4, double par6, float par8, int blockDamage, float partialTick) {
+    public void render(TileAlchemyFurnaceAdvanced tile, double x, double y, double z, float par8, int blockDamage, float partialTick) {
+        super.render(tile, x, y, z,par8, blockDamage, partialTick);
         int a;
         GL11.glPushMatrix();
-        GL11.glTranslatef((float)par2 + 0.5f, (float)par4, (float)par6 + 0.5f);
+        GL11.glTranslatef((float)x + 0.5f, (float)y, (float)z + 0.5f);
         GL11.glRotatef(90.0f, -1.0f, 0.0f, 0.0f);
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         if (tile.heat <= 100) {
@@ -42,23 +51,33 @@ public class TileAlchemyFurnaceAdvancedRenderer extends TileEntitySpecialRendere
             GL11.glPushMatrix();
             GL11.glTranslatef(0.5f, -0.5f, 1.1f);
             GL11.glRotatef(180.0f, 0.0f, 1.0f, 0.0f);
+            renderQuadFromIcon(BlocksTC.fluxGoo.getDefaultState(), 190, 0.0f);
             GL11.glPopMatrix();
+            GL11.glPushMatrix();
             float f = 1.0f - (float)tile.vis / (float)tile.maxVis;
             for (int a2 = 0; a2 < 4; ++a2) {
                 GL11.glPushMatrix();
+                GL11.glPushMatrix();
                 GL11.glRotatef((float)(90 * a2), 0.0f, 0.0f, 1.0f);
                 GL11.glRotatef(90.0f, -1.0f, 0.0f, 0.0f);
-                GL11.glTranslatef(0.85f, -1.8f, -1.4f);
-                GL11.glScaled(0.3, 0.6, 1.0);
+                GL11.glTranslatef(0.85f, -1.8f, -1.25f);
+                GL11.glScaled(0.4, 0.8, 0.4);
+                //this.renderQuadFromIcon(tile.getBlockType().getDefaultState(), 150, 0.0f);
                 GL11.glTranslatef(0.0f, 0.0f, -0.01f);
+                this.renderQuadFromIcon(BlocksTC.fluxGoo.getDefaultState(), 190, f);
                 GL11.glPopMatrix();
-                GL11.glPushMatrix();
-                GL11.glRotatef((float)(90 * a2), 0.0f, 0.0f, -1.0f);
-                GL11.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-                GL11.glTranslatef(1.15f, 1.8f, -1.4f);
-                GL11.glScaled(-0.3, -0.6, -1.0);
+                //GL11.glPushMatrix();
+                //GL11.glRotatef((float)(90 * a2), 0.0f, 0.0f, -1.0f);
+                //GL11.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+                //GL11.glTranslatef(1.15f, 1.8f, -1.4f);
+                //GL11.glScaled(-0.6, -0.6, -0.6);
+                //this.renderQuadFromIcon(tile.getBlockType().getDefaultState(), 150, 0.0f);
+                //GL11.glTranslatef(0.0f, 0.0f, 0.01f);
+                //this.renderQuadFromIcon(BlocksTC.fluxGoo.getDefaultState(), 190, f);
+                //GL11.glPopMatrix();
                 GL11.glPopMatrix();
             }
+            GL11.glPopMatrix();
         }
         if (tile.heat > 100) {
             GL11.glPushMatrix();
@@ -69,11 +88,30 @@ public class TileAlchemyFurnaceAdvancedRenderer extends TileEntitySpecialRendere
                 GL11.glRotatef(135.0f, 1.0f, 0.0f, 0.0f);
                 GL11.glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
                 GL11.glTranslatef(-0.5f, 0.0f, -1.0f);
+                GL11.glPushMatrix();
+                //GL11.glRotatef(180.0f, 1.0f, 0.0f, 0.0f);
+                GL11.glRotatef(180.0f, 1.0f, 0.0f, 0.0f);
+                GL11.glTranslated(0, -1, -1);
+                GL11.glScaled(1, 0.5, 1);
+                this.renderQuadFromIcon(Blocks.FIRE.getDefaultState(), 220, 1.0f - Math.min(1.0f, (float)tile.heat / (float)tile.maxPower));
+                GL11.glPopMatrix();
+                GL11.glTranslatef(0.0f, 0.0f, 0.05f);
+                //this.renderQuadFromIcon(tile.getBlockType().getDefaultState(), 150, 0.0f);
                 GL11.glPopMatrix();
             }
             GL11.glPopMatrix();
         }
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         GL11.glPopMatrix();
+    }
+
+    public void renderQuadFromIcon(IBlockState state, int brightness, float width) {
+        Minecraft mc = Minecraft.getMinecraft();
+        BlockRendererDispatcher dispatcher = mc.getBlockRendererDispatcher();
+        BlockModelShapes shapes = dispatcher.getBlockModelShapes();
+        IBakedModel thisBlock = shapes.getModelForState(state);
+        mc.renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        dispatcher.getBlockModelRenderer().renderModelBrightnessColor(state, thisBlock, 1f, 1f, 1f, 1f);
+
     }
 }
